6dda671eed3ec3be865395c18af4f716a6bd8cd5
Daniel Tull <dt@danieltull.co.uk>
2013-02-06 09:25:20 +0000

    Initial Commit

---
+++ SplitTabBarTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SplitTabBarTest
+//
+//  Created by Daniel Tull on 06.02.2013.
+//  Copyright (c) 2013 Daniel Tull. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
